5dae70870858b76496ac40ca03977da6239acfd5
RajashekarDandamudi <RajasekharDandamudi916@gmail.com>
2022-07-21 20:35:58 +0530

    21/07/2022

---
+++ calculator.py
@@ -0,0 +1,14 @@
+a = input()
+b = int(input())
+c = int(input())
+
+if (a == "+"):
+    print(c + b)
+elif (a == "-"):
+    print(b - c)
+elif (a == "*"):
+    print(b * c)
+elif (a == "/"):
+    print(b / c)
+elif (a == "%"):
+    print(b % c)
